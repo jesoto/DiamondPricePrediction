795568b84ae57598b30a1d5fd95758e4d0f08f42
Jesús Soto <jesus.soto@pucp.edu.pe>
2023-07-14 19:30:13 -0500

    Update training_pipelines.py

--- src/pipelines/training_pipelines.py
+++ src/pipelines/training_pipelines.py
@@ -4,11 +4,6 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 
-from src.components.data_ingestion import DataIngestion
-
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
-
 
 if __name__=='__main__':
     obj=DataIngestion()
